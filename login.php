65dd6719281f99fc1a0f851b3008ad6d86007629
Alfian Hamdani <alfian.hamdani04@gmail.com>
2020-10-16 22:09:19 +0800

    Add form to login.php

--- login.php
+++ login.php
@@ -11,20 +11,29 @@
 
 <body>
     <div class="cont">
+        <!--LOG IN-->
         <div class="form sign-in">
         <br>
         <br>
             <h2>Login</h2>
-            <label>
-        <span>Nama</span>
-        <input type="nama" name="nama">
-      </label>
-            <label>
-        <span>Password</span>
-        <input type="password" name="password">
-      </label>
-            <button class="submit" type="button">Login</button>
+            <form action="" method="post">
+                <!--NAMA-->
+                <label>
+                    <span>NIM/NIP</span>
+                    <input type="text" name="nim_nip" required>
+                </label>
 
+                <!--PASSWORD-->
+                <label>
+                    <span>Password</span>
+                    <input type="password" name="password" required>
+                </label>
+
+                <!--SUBMIT-->
+                <button class="submit">
+                    <input type="submit" value="LOGIN">
+                </button>
+            </form>
         </div>
 
         <div class="sub-cont">
@@ -44,23 +53,41 @@
                     <span class="m-in">Login</span>
                 </div>
             </div>
-            
+
+            <!--SIGN UP-->
             <div class="form sign-up" id="signup">
                 <div class="signup"></div>
                 <h2>Sign up</h2>
-                <label>
-                    <span>Name</span>
-                    <input type="text">
-                </label>
-                <label>
-                    <span>NIM</span>
-                    <input type="text">
-                </label>
-                <label>
-                    <span>Password</span>
-                    <input type="password">
-                </label>
-                <button type="button" class="submit" id="sign-up">Sign Up Now</button>
+                <form action="" method="post">
+                    <!--NAMA-->
+                    <label>
+                        <span>Nama</span>
+                        <input type="text" name="nama">
+                    </label>
+
+                    <!--NIM-->
+                    <label>
+                        <span>NIM/NIP</span>
+                        <input type="text" name="nim_nip">
+                    </label>
+
+                    <!--PASSWORD-->
+                    <label>
+                        <span>Password</span>
+                        <input type="password" name="password">
+                    </label>
+
+                    <!--CONFIRM PASSWORD-->
+                    <label>
+                        <span>Confirm Password</span>
+                        <input type="password" name="password2">
+                    </label>
+
+                    <!--SUBMIT-->
+                    <button class="submit" id="sign-up">
+                        <input type="submit" value="SIGN UP">
+                    </button>
+                </form>
             </div>
         </div>
     </div>
