ac631ca52721b05298d5d780c317b51e94655295
Alfian Hamdani <alfian.hamdani04@gmail.com>
2020-11-02 17:52:13 +0800

    Add QRCode Scanner

--- user/mahasiswa/dashboardmahasiswa.php
+++ user/mahasiswa/dashboardmahasiswa.php
@@ -41,7 +41,7 @@ if($notification){
 
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0; maximum-scale=1.0; user-scalable=0;">
     <meta name="description" content="">
     <meta name="author" content="Dashboard">
     <meta name="keyword" content="Dashboard, Bootstrap, Admin, Template, Theme, Responsive, Fluid, Retina">
@@ -61,7 +61,7 @@ if($notification){
     <link href="../../css/style.css" rel="stylesheet">
     <link href="../../css/style-responsive.css" rel="stylesheet">
     <script src="../../lib/chart-master/Chart.js"></script>
-
+    <script src="https://rawgit.com/sitepoint-editors/jsqrcode/master/src/qr_packed.js"></script>
     <!-- =======================================================
     Template Name: Dashio
     Template URL: https://templatemag.com/dashio-bootstrap-admin-template/
@@ -71,65 +71,73 @@ if($notification){
 </head>
 
 <body>
-    <section id="container">
-        <!-- **********************************************************************************************************************************************************
-        TOP BAR CONTENT & NOTIFICATIONS
-        *********************************************************************************************************************************************************** -->
-        <!--header start-->
-        <header class="header black-bg">
-            <div class="sidebar-toggle-box">
-                <div class="fa fa-bars tooltips" data-placement="right" data-original-title="Toggle Navigation"></div>
-            </div>
-            <!--logo start-->
-            <a href="dashboardmahasiswa.php" class="logo"><b>DASHBOARD <span>MAHASISWA</span></b></a>
-            <!--logo end-->
-            <div class="top-menu">
-                <ul class="nav pull-right top-menu">
-                    <li><a class="logout" href="../logout.php">Logout</a></li>
-                </ul>
-            </div>
-        </header>
-        <!--header end-->
-        <!-- **********************************************************************************************************************************************************
-        MAIN SIDEBAR MENU
-        *********************************************************************************************************************************************************** -->
-        <!--sidebar start-->
-        <aside>
-            <div id="sidebar" class="nav-collapse ">
-                <!-- sidebar menu start-->
-                <ul class="sidebar-menu" id="nav-accordion">
-                    <p class="centered">
-                        <img src="../../assets/img/profil.jpg" class="img-circle" width="80">
-                    </p><br>
-                    <h5 class="centered"><?php echo $userdata['nama_lengkap']?></h5>
-                    <h5 class="centered"><?php echo $userdata['nim']?></h5><br><br>
+<section id="container">
+    <!-- **********************************************************************************************************************************************************
+    TOP BAR CONTENT & NOTIFICATIONS
+    *********************************************************************************************************************************************************** -->
+    <!--header start-->
+    <header class="header black-bg">
+        <div class="sidebar-toggle-box">
+            <div class="fa fa-bars tooltips" data-placement="right" data-original-title="Toggle Navigation"></div>
+        </div>
+        <!--logo start-->
+        <a href="dashboardmahasiswa.php" class="logo"><b>DASHBOARD <span>MAHASISWA</span></b></a>
+        <!--logo end-->
+        <div class="top-menu">
+            <ul class="nav pull-right top-menu">
+                <li><a class="logout" href="../logout.php">Logout</a></li>
+            </ul>
+        </div>
+    </header>
+    <!--header end-->
+    <!-- **********************************************************************************************************************************************************
+    MAIN SIDEBAR MENU
+    *********************************************************************************************************************************************************** -->
+    <!--sidebar start-->
+    <aside>
+        <div id="sidebar" class="nav-collapse ">
+            <!-- sidebar menu start-->
+            <ul class="sidebar-menu" id="nav-accordion">
+                <p class="centered">
+                    <img src="../../assets/img/profil.jpg" class="img-circle" width="80">
+                </p><br>
+                <h5 class="centered"><?php echo $userdata['nama_lengkap']?></h5>
+                <h5 class="centered"><?php echo $userdata['nim']?></h5><br><br>
 
-                </ul>
-                <!-- sidebar menu end-->
-            </div>
-        </aside>
-        <!--sidebar end-->
-        <!-- **********************************************************************************************************************************************************
-        MAIN CONTENT
-        *********************************************************************************************************************************************************** -->
-        <!--main content start-->
-        <section id="main-content"> 
-            <section class="wrapper">
-                <!--  TODO PANEL -->
-                <!-- /col-md-4 --> 
-                <br><br>
-                <div class="col-md-12 col-sm-4 mb">
+            </ul>
+            <!-- sidebar menu end-->
+        </div>
+    </aside>
+    <!--sidebar end-->
+    <!-- **********************************************************************************************************************************************************
+    MAIN CONTENT
+    *********************************************************************************************************************************************************** -->
+    <!--main content start-->
+    <section id="main-content">
+        <section class="wrapper">
+            <!-- /col-md-4 -->
+            <br><br>
+            <div class="col-md-12 col-sm-4 mb">
                 <div class="green-panel pn">
-                  <div class="green-header">
-                    <h5>Silahkan Scan</h5>
-                  </div>
-                  <p><img src="../../img/code.png" width="100"></p>
-                  <h3>@Keterangan Hasil Scan</h3>
-                  <button class="btn btn-small btn-theme03">SUBMIT</button>
+                    <div class="green-header">
+                        <h5>Silakan Scan</h5>
+                    </div>
+                    <p>
+                        <a id="btn-scan-qr">
+                            <img src="../../img/code.png" width="100">
+                        </a>
+                    </p>
+
+                    <canvas hidden="" id="qr-canvas"></canvas>
+                    <div id="qr-result" hidden="">
+                        <b>Data:</b>
+                        <span id="outputData"></span>
+                        <button class="btn btn-small btn-theme03">SUBMIT</button>
+                    </div>
                 </div>
-              </div>
-            </section>
+            </div>
         </section>
+    </section>
     <!--main content end-->
 
     <!--footer start-->
@@ -152,62 +160,63 @@ if($notification){
         </div>
     </footer>
     <!--footer end-->
-    </section>
-    <!-- js placed at the end of the document so the pages load faster -->
-    <script src="../../lib/jquery/jquery.min.js"></script>
+</section>
+<!-- js placed at the end of the document so the pages load faster -->
+<script src="qrCodeScanner.js"></script>
+<script src="../../lib/jquery/jquery.min.js"></script>
 
-    <script src="../../lib/bootstrap/js/bootstrap.min.js"></script>
-    <script class="include" type="text/javascript" src="../../lib/jquery.dcjqaccordion.2.7.js"></script>
-    <script src="../../lib/jquery.scrollTo.min.js"></script>
-    <script src="../../lib/jquery.nicescroll.js" type="text/javascript"></script>
-    <script src="../../lib/jquery.sparkline.js"></script>
-    <!--common script for all pages-->
-    <script src="../../lib/common-scripts.js"></script>
-    <script type="text/javascript" src="../../lib/gritter/js/jquery.gritter.js"></script>
-    <script type="text/javascript" src="../../lib/gritter-conf.js"></script>
-    <!--script for this page-->
-    <script src="../../lib/sparkline-chart.js"></script>
-    <script src="../../lib/zabuto_calendar.js"></script>
-    <script type="application/javascript">
-        $(document).ready(function() {
-            $("#date-popover").popover({
-                html: true,
-                trigger: "manual"
-            });
-            $("#date-popover").hide();
-            $("#date-popover").click(function(e) {
-                $(this).hide();
-            });
-
-            $("#my-calendar").zabuto_calendar({
-                action: function() {
-                    return myDateFunction(this.id, false);
-                },
-                action_nav: function() {
-                    return myNavFunction(this.id);
-                },
-                ajax: {
-                    url: "show_data.php?action=1",
-                    modal: true
-                },
-                legend: [{
-                    type: "text",
-                    label: "Special event",
-                    badge: "00"
-                }, {
-                    type: "block",
-                    label: "Regular event",
-                }]
-            });
+<script src="../../lib/bootstrap/js/bootstrap.min.js"></script>
+<script class="include" type="text/javascript" src="../../lib/jquery.dcjqaccordion.2.7.js"></script>
+<script src="../../lib/jquery.scrollTo.min.js"></script>
+<script src="../../lib/jquery.nicescroll.js" type="text/javascript"></script>
+<script src="../../lib/jquery.sparkline.js"></script>
+<!--common script for all pages-->
+<script src="../../lib/common-scripts.js"></script>
+<script type="text/javascript" src="../../lib/gritter/js/jquery.gritter.js"></script>
+<script type="text/javascript" src="../../lib/gritter-conf.js"></script>
+<!--script for this page-->
+<script src="../../lib/sparkline-chart.js"></script>
+<script src="../../lib/zabuto_calendar.js"></script>
+<script type="application/javascript">
+    $(document).ready(function() {
+        $("#date-popover").popover({
+            html: true,
+            trigger: "manual"
+        });
+        $("#date-popover").hide();
+        $("#date-popover").click(function(e) {
+            $(this).hide();
         });
 
-        function myNavFunction(id) {
-            $("#date-popover").hide();
-            var nav = $("#" + id).data("navigation");
-            var to = $("#" + id).data("to");
-            console.log('nav ' + nav + ' to: ' + to.month + '/' + to.year);
-        }
-    </script>
+        $("#my-calendar").zabuto_calendar({
+            action: function() {
+                return myDateFunction(this.id, false);
+            },
+            action_nav: function() {
+                return myNavFunction(this.id);
+            },
+            ajax: {
+                url: "show_data.php?action=1",
+                modal: true
+            },
+            legend: [{
+                type: "text",
+                label: "Special event",
+                badge: "00"
+            }, {
+                type: "block",
+                label: "Regular event",
+            }]
+        });
+    });
+
+    function myNavFunction(id) {
+        $("#date-popover").hide();
+        var nav = $("#" + id).data("navigation");
+        var to = $("#" + id).data("to");
+        console.log('nav ' + nav + ' to: ' + to.month + '/' + to.year);
+    }
+</script>
 </body>
 
 </html>
